d9a2974610a29898451a96842a57705a803a7e21
PraveenHegde-Deloitte <PraveenHegde-Deloitte@users.noreply.github.com>
2022-04-05 18:32:05 +0530

    3rd assignment python

---
+++ Mini_Assignment_3/F.py
@@ -0,0 +1,12 @@
+#changing city Name
+
+sampleDict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+v=sampleDict['city']
+del sampleDict['city']
+sampleDict['Location']=v
+print(sampleDict)
